a0d973e572bdab0b65a65521812c877c3bff50f2
HiBjhs <1911140011x@ed.fuk.kindai.ac.jp>
2021-12-08 14:14:59 +0900

    add sete-ubuntu

---
+++ sete-ubuntu/README.md
@@ -0,0 +1,4 @@
+# Ubuntu入れてやる事
+Mozcを入れて日本語化  
+saisho.shを実行  
+デフォルトshellの変更  
---
+++ sete-ubuntu/saisho.sh
@@ -0,0 +1,13 @@
+ #!/bin/sh
+ LANG=C xdg-user-dirs-gtk-update
+ sudo apt update
+ sudo apt upgrade -y
+ sudo apt autoremove
+ sudo apt install git
+ sudo apt install 
+ sudo apt install preload -y
+ sudo apt-get install gnome-tweaks -y
+ sudo apt install zeal -y
+ sudo apt install snapd
+ sudo snap install postman
+
