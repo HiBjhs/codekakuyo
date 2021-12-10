13ec1b496f14a70f65529831abc2e55c774caa7c
HiBjhs <1911140011x@ed.fuk.kindai.ac.jp>
2021-12-10 15:26:13 +0900

    add vim_script

--- sete-ubuntu/saisho.sh
+++ sete-ubuntu/saisho.sh
@@ -8,7 +8,8 @@
  sudo apt install preload -y
  sudo apt-get install gnome-tweaks -y
  sudo apt install zeal -y
- sudo apt install snapd
- sudo snap install postman
- sudo apt install screen
- sudo apt install curl
+ sudo apt install snapd -y
+ sudo snap install postman-y
+ sudo apt install screen -y
+ sudo apt install curl -y
+ sudo apt install vim -y
